3882f5b572d042db978a3836a75783d65c54383e
EricLauterbach <97121105+EricLauterbach@users.noreply.github.com>
2026-03-03 23:56:46 -0800

    Update global.js
    up

--- global.js
+++ global.js
@@ -1261,7 +1261,7 @@ function initCopyleaksMarketing() {
     focus: '1',
     arrows: false,
     pagination: false,
-    autoWidth: false,
+    autoWidth: true,
     fixedWidth: '400px',
     autoplay: false,
     snap: true,
